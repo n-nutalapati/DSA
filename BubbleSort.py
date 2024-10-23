59d3c149bccfc95fe5b3a3c4cb658011065f6aa5
nnutalap <nnutalap@NNUTALAP-7420.in.oracle.com>
2024-10-23 22:54:30 +0530

    Initial Commit

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,6 +1,6 @@
 def bubbleSort(arr):
     for i in range(len(arr)):
-        for j in range(0, len(arr)-1):
+        for j in range(0, len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
